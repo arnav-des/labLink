c9f8852d17e67a2786d11dcb8c506ce79120fc5a
Ankit kumar <ankit.kumar.info22@gmail.com>
2023-02-24 22:16:19 +0530

    folderstructure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./HomePage.css"
+
+function HomePage() {
+  return (
+<div className='homeMain'>
+    <div className='certificateMain'>
+
+    </div>
+    <div className='loginMain'>
+        <button className='loginEngineer'>
+            Login For Engineer!
+        </button>
+
+    </div>
+
+</div>
+
+    )
+}
+
+export default HomePage
