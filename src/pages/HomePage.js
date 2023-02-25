fc9fa8e7905c37ed554598bf0ba1c04da0699b63
Khushi <kb5122@srmist.edu.in>
2023-02-25 11:00:46 +0530

    certi update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,104 +1,115 @@
-
-import React, { useState,useEffect } from 'react'
-import { auth, provider,db } from "../firebase";
+import React, { useState, useEffect } from "react";
+import { auth, provider, db } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
-import { collection, getDocs} from "firebase/firestore";
-import moment from 'moment'
-import "./HomePage.css"
+import { collection, getDocs } from "firebase/firestore";
+import moment from "moment";
 
+import "./HomePage.css";
 
-function HomePage({setIsAuth,isAuth}) {
- 
-  const[userData,setUserData]=useState({});
+function HomePage({ setIsAuth, isAuth }) {
+  const [userData, setUserData] = useState({});
 
-
-  const [dataList,setDataList]=useState([]);
+  const [dataList, setDataList] = useState([]);
   const dataCollectionRef = collection(db, "machineData");
- 
+
   useEffect(() => {
     const getList = async () => {
       const data = await getDocs(dataCollectionRef);
       setDataList(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })));
     };
 
-  
-
     getList();
   }, []);
 
- console.log(dataList);
+  console.log(dataList);
 
+  const navigate = useNavigate();
 
-
-    const navigate = useNavigate();
-
-    const signInWithGoogle = async () => {
-      const result = await signInWithPopup(auth, provider);
-      console.log(result);
-      if(result){
-        setIsAuth(true);
-        localStorage.setItem("isAuth",true);
-        setUserData(result);
-        navigate("/details");
-      }
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    if (result) {
+      setIsAuth(true);
+      localStorage.setItem("isAuth", true);
+      setUserData(result);
+      navigate("/details");
     }
+  };
   return (
     <div className="homeMain">
       <div className="certificateMain">
+        <h1 className="certificateHead">
+          {" "}
+          <img
+            classname="logo"
+            src={require("../img/logo.png")}
+            width="80px"
+            height={"80px"}
+          />
+          Certificate{" "}
+        </h1>
 
-    <h1 className='certificateHead'>Certificate</h1>
-    <div>
-      {
-        dataList?.map((certificate)=>{
-
-          return(
-          <>
-         
-              <div>
-            <h3>Manufacturer: {certificate.manufacturer}</h3>
-            <h3>Liscense: {certificate.liscense}</h3>
-            <h3>Model Number: {certificate.model}</h3>
-            <h3>Owner: {certificate.owner}</h3>
-            <h3>Purchase Date: {moment(certificate.purchaseDate).format("MMM Do YY")}</h3>
-            <h3>Last Service Date: {moment(certificate.lastService).format("MMM Do YY")}</h3>
-            <h3>Next Service Date: {moment(certificate.nextService).format("MMM Do YY")}</h3>
-            
-              </div>
-          
-          
-          </>
-          );
-        })
-      }
-    </div>
-
+        <div>
+          {dataList?.map((certificate) => {
+            return (
+              <>
+                <div>
+                  <div>
+                    <h3 className="details">Manufacturer: </h3>
+                    <h4>{certificate.manufacturer}</h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Liscense:</h3>
+                    <h4> {certificate.liscense}</h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Model Number:</h3>
+                    <h4> {certificate.model}</h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Owner:</h3>
+                    <h4> {certificate.owner}</h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Purchase Date: </h3>
+                    <h4>
+                      {" "}
+                      {moment(certificate.purchaseDate).format("MMM Do YY")}
+                    </h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Last Service Date: </h3>
+                    <h4>
+                      {moment(certificate.lastService).format("MMM Do YY")}
+                    </h4>
+                  </div>
+                  <div>
+                    <h3 className="details">Next Service Date: </h3>
+                    <h4>
+                      {moment(certificate.nextService).format("MMM Do YY")}
+                    </h4>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
 
-      
-
-    <div className='loginMain'>
-        <button className='loginEngineer' onClick={signInWithGoogle}>
-            Login For Engineer!
+      <div className="loginMain">
+        <button className="loginEngineer" onClick={signInWithGoogle}>
+          Login For Engineer!
         </button>
+      </div>
 
-    </div>
-    
-      
-
-    <div className='loginMain'>
-        <Link to="/approve" className='loginEngineer'  >
-
-            Get Certified by NABL
+      <div className="loginMain">
+        <Link to="/approve" className="loginEngineer">
+          Get Certified by NABL
         </Link>
-
+      </div>
     </div>
-    
-
-</div>
-
-    )
-
+  );
 }
 
 export default HomePage;
